def809a396c7a91bb7b9f1fe0b4b4341da451016
Bhagyashri Pagar <73631176+Bhagyashri-pagar@users.noreply.github.com>
2023-08-21 22:08:05 +0530

    Add files via upload

---
+++ Accept_Print.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Accept_Print {
+    public static void main(String[] args)
+    {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Accept 3 integer values from user:");
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+
+        System.out.println("Integer values are:");
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+
+
+    }
+}
